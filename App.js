87fd3ea65230da67a673751497e9ef869f3c8316
Tanmay Kaushik <96286116+tanmaykaushikk@users.noreply.github.com>
2025-04-02 11:29:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,23 +7,25 @@ import Successful from "./src/screens/Successful.tsx";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 import Search from "./src/screens/Search.tsx";
+import SignupScreen from "./src/screens/SignupScreen.tsx";
+import LoginScreen from "./src/screens/LoginScreen.tsx";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-       <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Landing" component={Landing} />
-        <Stack.Screen name="Wishlist" component={Wishlist} />
-        <Stack.Screen name="Cart" component={Cart} />
-        <Stack.Screen name="Successful" component={Successful} />
-        <Stack.Screen name="Search" component={Search} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </Provider>
-
-   
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Signup" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Landing" component={Landing} />
+          <Stack.Screen name="Wishlist" component={Wishlist} />
+          <Stack.Screen name="Cart" component={Cart} />
+          <Stack.Screen name="Successful" component={Successful} />
+          <Stack.Screen name="Search" component={Search} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
